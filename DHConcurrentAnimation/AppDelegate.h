2addb7589769fbe4c89c9b98c1753a62b4b42709
hhuang <hhuang@microstrategy.com>
2016-09-07 09:28:48 +0800

    Initial Commit

---
+++ DHConcurrentAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHConcurrentAnimation
+//
+//  Created by Huang Hongsen on 9/7/16.
+//  Copyright © 2016 cn.daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
